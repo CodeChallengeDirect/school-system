6d14f85dfe1faf4998d8988428f85ab82b5b5378
DavidKMuchiri <dkamau716@gmail.com>
2023-10-31 20:42:32 +0300

    Removing static swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,16 +4,11 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { config } from 'dotenv';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
 
 config();
 
 @Module({
-  imports: [UsersModule, MongooseModule.forRoot(process.env.MONGO_DB_CONNECTION_STRING), ServeStaticModule.forRoot({
-    rootPath: join(__dirname, '..', 'swagger-static'),
-    serveRoot: process.env.NODE_ENV === 'development' ? '/' : '/swagger',
-  })],
+  imports: [UsersModule, MongooseModule.forRoot(process.env.MONGO_DB_CONNECTION_STRING)],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-// import * as path from 'path';
-import { resolve } from 'path';
-import { writeFileSync } from 'fs';
-import { config } from 'dotenv';
-
-config()
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -18,20 +12,7 @@ async function bootstrap() {
     .addTag('API')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('/swagger', app, document, {customCssUrl: CSS_URL});
-
-  if (process.env.NODE_ENV === 'development') {
-    const pathToSwaggerStaticFolder = resolve(process.cwd(), 'swagger-static');
-
-    // write swagger json file
-    const pathToSwaggerJson = resolve(
-      pathToSwaggerStaticFolder,
-      '../swagger.json',
-    );
-    const swaggerJson = JSON.stringify(document, null, 2);
-    writeFileSync(pathToSwaggerJson, swaggerJson);
-    console.log(`Swagger JSON file written to: ${pathToSwaggerJson}`);
-  }
+  SwaggerModule.setup('/docs', app, document, {customCssUrl: CSS_URL});
 
   await app.listen(5000);
 }
