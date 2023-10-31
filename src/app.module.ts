6ea8f9d9ce2f0c202d9a861a93e9422f3a8a8401
DavidKMuchiri <dkamau716@gmail.com>
2023-10-31 20:29:10 +0300

    Trying to fix swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,16 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { config } from 'dotenv';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 config();
 
 @Module({
-  imports: [UsersModule, MongooseModule.forRoot(process.env.MONGO_DB_CONNECTION_STRING)],
+  imports: [UsersModule, MongooseModule.forRoot(process.env.MONGO_DB_CONNECTION_STRING), ServeStaticModule.forRoot({
+    rootPath: join(__dirname, '..', 'swagger-static'),
+    serveRoot: process.env.NODE_ENV === 'development' ? '/' : '/swagger',
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+// import * as path from 'path';
+import { resolve } from 'path';
+import { writeFileSync } from 'fs';
+import { config } from 'dotenv';
+config()
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,7 +17,20 @@ async function bootstrap() {
     .addTag('API')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document, {customCssUrl: CSS_URL});
+  SwaggerModule.setup('/swagger', app, document, {customCssUrl: CSS_URL});
+
+  if (process.env.NODE_ENV === 'development') {
+    const pathToSwaggerStaticFolder = resolve(process.cwd(), 'swagger-static');
+
+    // write swagger json file
+    const pathToSwaggerJson = resolve(
+      pathToSwaggerStaticFolder,
+      '../swagger.json',
+    );
+    const swaggerJson = JSON.stringify(document, null, 2);
+    writeFileSync(pathToSwaggerJson, swaggerJson);
+    console.log(`Swagger JSON file written to: ${pathToSwaggerJson}`);
+  }
 
   await app.listen(5000);
 }
